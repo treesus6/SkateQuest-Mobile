fda5effed7d607c2be654ddbdbd590337f6de779
treesus6 <treevanderveer@gmail.com>
2026-01-17 21:37:20 -0800

    Add Mapbox repository configuration for Gradle

---
+++ plugins/withMapboxRepo.js
@@ -0,0 +1,25 @@
+const { withProjectBuildGradle } = require('@expo/config-plugins');
+
+module.exports = function withMapboxRepo(config) {
+  return withProjectBuildGradle(config, (config) => {
+    if (config.modResults.language === 'groovy') {
+      config.modResults.contents = config.modResults.contents.replace(
+        /allprojects\s*{/,
+        `allprojects {
+    repositories {
+        maven {
+            url 'https://api.mapbox.com/downloads/v2/releases/maven'
+            authentication {
+                basic(BasicAuthentication)
+            }
+            credentials {
+                username = 'mapbox'
+                password = System.getenv("RNMAPBOX_MAPS_DOWNLOAD_TOKEN")
+            }
+        }
+    }`
+      );
+    }
+    return config;
+  });
+};
