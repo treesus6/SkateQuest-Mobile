e2d01da61545c81918908a510c9bc49e38be9c44
treesus6 <treevanderveer@gmail.com>
2026-03-21 08:10:00 -0700

    fix: add RN imports to App.tsx remove require

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { View, ActivityIndicator } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import * as SystemUI from 'expo-system-ui';
 import AsyncStorage from '@react-native-async-storage/async-storage';
