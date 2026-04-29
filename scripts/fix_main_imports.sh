c12502a9ea71dce8f7a0ee72c5f9b312b536f74e
Tyler Vanderveer <treevanderveer@gmail.com>
2026-04-29 07:50:37 -0700

    chore: remove dead file

--- scripts/fix_main_imports.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Fix the missing Firebase imports in main.js
-sed -i '886s/const { db, getDocs, collectionGroup } = window.firebaseInstances;/const { db, getDocs, collectionGroup, doc, setDoc } = window.firebaseInstances;/' main.js
-echo "Fixed Firebase imports in main.js"
