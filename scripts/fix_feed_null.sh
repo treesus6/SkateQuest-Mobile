14fbc7f31c0e8553019582bfed61a94ed02b8c02
Tyler Vanderveer <treevanderveer@gmail.com>
2026-04-29 07:50:36 -0700

    chore: remove dead file

--- scripts/fix_feed_null.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Add null check around the feed operations
-sed -i '774s/if (feed) feed.innerHTML/if (!feed) return;\n            feed.innerHTML/' main.js
-echo "Fixed feed null check"
