73da7c2039a3eb5e0693fc8a54151669d6498368
Tyler Vanderveer <treevanderveer@gmail.com>
2026-08-18 09:23:17 -0700

    Block anonymous SkateTV mutations

---
+++ supabase/migrations/20260818164604_block_anon_skatetv_mutations.sql
@@ -0,0 +1,3 @@
+revoke insert, update, delete on public.skatetv_clips from anon;
+revoke insert, update, delete on public.skatetv_likes from anon;
+revoke insert, update, delete on public.skatetv_views from anon;
