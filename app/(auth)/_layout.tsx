7ef0bc907aaeaa8de017477cd19acf779d6a5d69
Tyler Vanderveer <treevanderveer@gmail.com>
2026-08-18 06:52:39 -0700

    Register auth callback route in stack

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -7,6 +7,7 @@ export default function AuthLayout() {
       <Stack.Screen name="signup" />
       <Stack.Screen name="forgot-password" />
       <Stack.Screen name="reset-password" />
+      <Stack.Screen name="callback" />
     </Stack>
   );
 }
